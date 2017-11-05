437eb8b796803bf86464a57882fc5b8c7dc50741
acruz1992 <andrew.cruz1@yahoo.com>
2017-11-04 22:32:51 -0700

    Signed-off-by: acruz1992 <andrew.cruz1@yahoo.com>

--- Proj/Project_1/main.cpp
+++ Proj/Project_1/main.cpp
@@ -24,13 +24,14 @@ int main(int argc, char** argv) {
     char action;
     int ctotal;
     char ch;
+    char hit;
     
     //Loop the Menu and Problems
     cout<<"How many games would you like to play?: ";
     cin>>choice;
     cin.ignore();
     do{
-        cout<<"Dealers hand: \n";
+        cout<<"Dealers hand:"<<endl;
         int card1=(rand()%(14-2+1))+2;  //dealer card 1
         int card2=(rand()%(14-2+1))+2;  //dealer card 2
         int card=(rand()%(14-2+1))+2;  //dealer card 3 to hit if needed
@@ -47,8 +48,6 @@ int main(int argc, char** argv) {
                     default: cout<<card1;
                     break;
                 }
-            if(card1<=14&&card1>=12)
-                card1=10;
             cout<<" and ";
                 switch (card2)
                 {
@@ -63,13 +62,48 @@ int main(int argc, char** argv) {
                     default: cout<<card2;
                     break;
                 }
+                if(card1<=14&&card1>=12)
+                    card1=10;
                 if(card2<=14&&card2>=12)
                 card2=10;
+                
                 ctotal=card1+card2;
-                cout<<"\nTotal is "<<ctotal<<"\n";
-                cout<<"Press Enter to continue\n";
-                ch=cin.get();
-                cout<<endl;
+                cout<<"\nTotal is "<<ctotal<<endl;
+                if (ctotal<=20)
+                {
+                    cout<<"\nWould you like to hit again? Y/N: ";
+                    cin>>hit;
+                    if (hit=='Y'||hit=='y')
+                    {
+                        cout<<card1<<" and "<<card2<<" and ";
+                        switch (card)
+                        {
+                            case 11: cout<<"Ace";
+                            break;
+                            case 12: cout<<"Jack";
+                            break;
+                            case 13: cout<<"Queen";
+                            break;
+                            case 14: cout<<"King";
+                            break;
+                            default: cout<<card;
+                            break;
+                        }
+                        if(card<=14&&card>=12)
+                        card=10;
+                        else if(card==11)
+                        card=1;
+                        ctotal+=card;
+                        cout<<"\nNew total is "<<ctotal<<"\n"<<endl;
+                        if(ctotal>21)
+                            {
+                            cout<<"\nYou loose!\n";
+                            cout<<endl;
+                            }
+                    }
+                    else if(hit=='N'||hit=='n');
+                }
+    
                 
                        // if (action=='H'||action=='h')
                     
