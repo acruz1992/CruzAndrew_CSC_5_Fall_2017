76640cc7fd96a0c1db53db4c3cca3c961d0a0ad0
acruz1992 <andrew.cruz1@yahoo.com>
2017-11-04 17:51:52 -0700

    Signed-off-by: acruz1992 <andrew.cruz1@yahoo.com>

---
+++ Proj/Project_1/main.cpp
@@ -0,0 +1,83 @@
+/* 
+ * File:   main.cpp
+ * Author: Andrew Cruz
+ * Created on Oct 28th, 2017, 9:00 AM
+ * Purpose:  Blackjack Game Project
+ */
+//System Libraries
+
+#include <iostream>     //Input/Output Stream Library
+#include <cstdlib>      //Library set random number seed
+#include <ctime>        //Libary to seed the random number generator
+#include <iomanip>      //Format Library
+#include <fstream>      //File Library
+#include <string.h>     //String Library
+using namespace std;    //Standard Name-space under which System Libraries reside
+    
+
+int main(int argc, char** argv) {
+
+    int choice;
+    srand(static_cast<unsigned int>(time(0)));
+    
+    //to deal values from 2-14
+    int card1=rand()%14+2;  //dealer card 1
+    int card2=rand()%14+2;  //dealer card 2
+    int card=rand()%14+2;  //dealer card 3 to hit if needed
+  
+    float wager;
+    float housewager;
+    float bets;
+    char action;
+    
+    //Loop the Menu and Problems
+    cout<<"How many games would you like to play?: ";
+    cin>>choice;
+    do{
+        
+        {
+           
+            
+                switch (card1)
+                {
+                    case 11: cout<<"Ace ";
+                    break;
+                    case 12: cout<<"Jack";
+                    break;
+                    case 13: cout<<"Queen ";
+                    break;
+                    case 14: cout<<"King ";
+                    break;
+                    default: cout<<card1;
+                    break;
+                }
+                switch (card2)
+                {
+                    case 11: cout<<"Ace ";
+                    break;
+                    case 12: cout<<"Jack";
+                    break;
+                    case 13: cout<<"Queen ";
+                    break;
+                    case 14: cout<<"King ";
+                    break;
+                    default: cout<<card2;
+                    break;
+                }
+                        
+                    }
+                    {
+                
+                
+                        if (action=='H'||action=='h')
+                        {
+
+                        }
+                    }
+                
+            
+        }  
+    }while(choice>=1)
+    
+    return 0;
+}
